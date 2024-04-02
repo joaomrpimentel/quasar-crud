034a1c65aead284cb5cac0d3f5ec94748ecec009
João Marcos <joaomrpimentel@gmail.com>
2024-04-02 20:07:02 -0300

    modified:   crud/package-lock.json 	modified:   crud/package.json 	modified:   crud/quasar.config.js 	modified:   crud/src/boot/axios.ts 	modified:   crud/src/components/EssentialLink.vue 	new file:   crud/src/composables/UseApi.ts 	modified:   crud/src/layouts/MainLayout.vue 	modified:   crud/src/pages/IndexPage.vue 	modified:   crud/src/router/routes.ts 	new file:   crud/src/services/posts.ts 	modified:   server/db.json

--- crud/src/layouts/MainLayout.vue
+++ crud/src/layouts/MainLayout.vue
@@ -28,7 +28,7 @@
         <q-item-label
           header
         >
-          Essential Links
+          Menus
         </q-item-label>
 
         <EssentialLink
@@ -55,46 +55,10 @@ defineOptions({
 
 const linksList: EssentialLinkProps[] = [
   {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
+    title: 'Home',
+    caption: ' ',
+    icon: 'home',
+    route: {name: 'home'}
   }
 ];
 
--- crud/src/pages/IndexPage.vue
+++ crud/src/pages/IndexPage.vue
@@ -1,47 +1,76 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+  <q-page padding>
+    <q-table
+      title="Treats"
+      :rows="posts"
+      :columns="colunas"
+      row-key="name"
+    >
+    <template v-slot:body-cell-actions="props">
+      <q-td :props="props">
+        <q-btn icon="delete" color="negative" dense size="sm" @click="handleDeletePost(props.row.id)" />
+      </q-td>
+      </template>
+    </q-table>
   </q-page>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
+import { defineComponent, ref, onMounted } from 'vue';
+import postsService from '../services/posts';
+import { useQuasar } from 'quasar';
 
-defineOptions({
-  name: 'IndexPage'
-});
+  const posts = ref([])
+  const { list, remove } = postsService()
+  const colunas = [
+    {name: 'id', field: 'id', label: 'id', sortable: true},
+    {name: 'title', field: 'title', label: 'Título', sortable: true},
+    {name: 'author', field: 'author', label: 'Autor', sortable: true},
+    {name: 'content', field: 'content', label: 'Conteúdo', sortable: true},
+    {name: 'actions', field: 'actions', label: 'Ações', sortable: true}
+  ]
 
-const todos = ref<Todo[]>([
-  {
-    id: 1,
-    content: 'ct1'
-  },
-  {
-    id: 2,
-    content: 'ct2'
-  },
-  {
-    id: 3,
-    content: 'ct3'
-  },
-  {
-    id: 4,
-    content: 'ct4'
-  },
-  {
-    id: 5,
-    content: 'ct5'
+    const $q = useQuasar()
+
+    onMounted(async () => {
+      getPosts()
+    })
+
+    const getPosts = async () => {
+      try {
+        const data = await list()
+        posts.value = data
+      } catch (error) {
+        console.error (error)
+      }
+    }
+
+    const handleDeletePost = async (id: any) => {
+      try {
+        $q.dialog({
+        title: 'Deletar?',
+        message: 'Deseja deletar esse post?',
+        cancel: true,
+        persistent: true
+        }).onOk(async () => {
+          await remove(id)
+          $q.notify({message: 'Apagado com sucesso', icon: 'check', color: 'positive'})
+          await getPosts()
+        })
+      } catch (error) {
+        $q.notify({message: 'Erro ao apagar', icon: 'times', color: 'negative'})
+      }
+    }
+
+defineComponent({
+  name: 'IndexPage',
+  setup () {
+    return {
+      posts,
+      colunas,
+      handleDeletePost
+    }
   }
-]);
-
-const meta = ref<Meta>({
-  totalCount: 1200
 });
+
 </script>
